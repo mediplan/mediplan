e74947fed234d87efd4065c5a32affd4302a9319
info <info@diagnostikamed.it>
2026-05-27 08:54:53 +0000

    File changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from '@/App.jsx'
 import '@/index.css'
 
+// Disregistra tutti i Service Worker e forza il reload per aggiornare la cache
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    registrations.forEach((registration) => registration.unregister());
+    if (registrations.length > 0) {
+      window.location.reload(true);
+    }
+  });
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App />
 )
